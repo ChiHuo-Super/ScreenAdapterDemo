2216a51cf643040064b95029c44e4bee609454ba
周生杰 <1946591274@qq.com>
2020-05-03 12:49:20 +0800

    基于今日头条屏幕适配方案实现

---
+++ app/src/main/java/com/demo/screenadapter/MainActivity.java
@@ -0,0 +1,15 @@
+package com.demo.screenadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ScreenAdapter.setCustomDensity(this,getApplication());
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/demo/screenadapter/ScreenAdapter.java
@@ -0,0 +1,61 @@
+package com.demo.screenadapter;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.content.res.Configuration;
+import android.util.DisplayMetrics;
+
+/**
+ * 基于今日头条屏幕适配方案
+ */
+public class ScreenAdapter {
+    // 系统的Density
+    private static float sNoncompatDensity;
+    // 系统的ScaledDensity
+    private static float sNoncompatScaledDensity;
+
+    public static void setCustomDensity(Activity activity, final Application application) {
+        DisplayMetrics displayMetrics = application.getResources().getDisplayMetrics();
+        if (sNoncompatDensity == 0) {
+            sNoncompatDensity = displayMetrics.density;
+            sNoncompatScaledDensity = displayMetrics.scaledDensity;
+            // 监听在系统设置中切换字体
+            application.registerComponentCallbacks(new ComponentCallbacks() {
+                @Override
+                public void onConfigurationChanged(Configuration newConfig) {
+                    if (newConfig != null && newConfig.fontScale > 0) {
+                        sNoncompatScaledDensity = application.getResources().getDisplayMetrics().scaledDensity;
+                    }
+                }
+
+                @Override
+                public void onLowMemory() {
+
+                }
+            });
+        }
+
+        // 此处以宽度1080dp的设计图作为例子
+        float targetDensity = displayMetrics.widthPixels / 1080.0f;
+        float targetScaledDensity = targetDensity * (sNoncompatScaledDensity / sNoncompatDensity);
+        int targetDensityDpi = (int) (160 * targetDensity);
+        displayMetrics.density = targetDensity;
+        displayMetrics.scaledDensity = targetScaledDensity;
+        displayMetrics.densityDpi = targetDensityDpi;
+
+        DisplayMetrics activityDisplayMetrics = activity.getResources().getDisplayMetrics();
+        activityDisplayMetrics.density = targetDensity;
+        activityDisplayMetrics.scaledDensity = targetScaledDensity;
+        activityDisplayMetrics.densityDpi = targetDensityDpi;
+    }
+    /**-----------------------------
+     *dpi	            屏幕密度
+     *drawable-ldpi	    0.75
+     *drawable-mdpi	    1(baseline)
+     *drawable-hdpi	    1.5
+     *drawable-xhdpi	2
+     *drawable-xxhdpi	3
+     *drawable-xxxdpi	4
+     -----------------------------*/
+}
